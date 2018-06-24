bb82bfeabb9621daebb62d76e05178383f1eb388
AlexTicovschi <alexmticovschi@gmail.com>
2018-06-24 17:09:52 +0100

    fetching users from an online API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 
 import './App.css';
 
 class App extends Component {
     state = {
-        robots: robots,
+        robots: [],
         searchfield: ''
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
@@ -20,13 +25,15 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        return (
-            <div className='tc'>      
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+
+        return this.state.robots.length === 0 ? 
+            <h1 className='tc'>Loading</h1> 
+            :   <div className='tc'>      
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div> 
+
     }
 }
 
